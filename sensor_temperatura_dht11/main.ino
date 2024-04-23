b4f7e1e31aeff2121e2dcf08fd4f02bc7f49aaec
Diego Reis <diegoportal.reis@outlook.com>
2024-04-23 13:55:32 -0300

    Projeto sensor de temperatura
    Sensor de temperatura utilizando o arduino uno com o dht11

---
+++ sensor_temperatura_dht11/main.ino
@@ -0,0 +1,42 @@
+#include "DHT.h"
+#include <ArduinoJson.h>
+ 
+#define DHTPIN A1 // pino que estamos conectado
+#define DHTTYPE DHT11 // DHT 11
+ 
+// Conecte pino 1 do sensor (esquerda) ao +5V
+// Conecte pino 2 do sensor ao pino de dados definido em seu Arduino
+// Conecte pino 4 do sensor ao GND
+// Conecte o resistor de 10K entre pin 2 (dados) 
+// e ao pino 1 (VCC) do sensor
+DHT dht(DHTPIN, DHTTYPE);
+ 
+void setup() 
+{
+  Serial.begin(9600);
+  dht.begin();
+}
+ 
+void loop() 
+{
+
+  StaticJsonDocument<200> doc;
+  // A leitura da temperatura e umidade pode levar 250ms!
+  // O atraso do sensor pode chegar a 2 segundos.
+  float h = dht.readHumidity();
+  float t = dht.readTemperature();
+
+  // testa se retorno é valido, caso contrário algo está errado.
+  if (isnan(t) || isnan(h)) 
+  {
+    Serial.println("Failed to read from DHT");
+  } 
+  else
+  {
+    doc["umidade"] = h;
+    doc["temperatura_c"] = t;
+    serializeJson(doc, Serial);
+    Serial.println();
+    delay(2 * 1000);
+  }
+}
